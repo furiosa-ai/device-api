8d8f60afe06594949640229238866bdc7cbdfff0
Yongwoo Lee <ywlee@furiosa.ai>
2023-04-06 15:06:56 +0900

    change from_env, from_str to classmethod

--- device-api-python/src/config/mod.rs
+++ device-api-python/src/config/mod.rs
@@ -4,9 +4,9 @@ mod inner;
 
 use std::str::FromStr;
 
-use env::EnvBuilderNotDeterminedPy;
 use furiosa_device::DeviceConfig;
 use pyo3::prelude::*;
+use pyo3::types::PyType;
 
 use crate::errors::to_py_err;
 use crate::{arch::ArchPy, device::DeviceModePy};
@@ -14,7 +14,7 @@ use crate::{arch::ArchPy, device::DeviceModePy};
 #[pyclass(name = "DeviceConfig")]
 #[derive(Clone)]
 pub struct DeviceConfigPy {
-    inner: DeviceConfig,
+    pub inner: DeviceConfig,
 }
 
 impl DeviceConfigPy {
@@ -39,16 +39,16 @@ impl DeviceConfigPy {
         DeviceConfigPy::new(config)
     }
 
-    #[staticmethod]
-    pub fn from_env(key: &str) -> PyResult<DeviceConfigPy> {
+    #[classmethod]
+    fn from_env(cls: &PyType, key: &str) -> PyResult<DeviceConfigPy> {
         DeviceConfig::from_env(key)
             .build()
             .map(DeviceConfigPy::new)
             .map_err(to_py_err)
     }
 
-    #[staticmethod]
-    pub fn from_str(key: &str) -> PyResult<DeviceConfigPy> {
+    #[classmethod]
+    fn from_str(cls: &PyType, key: &str) -> PyResult<DeviceConfigPy> {
         DeviceConfig::from_str(key)
             .map(DeviceConfigPy::new)
             .map_err(to_py_err)
