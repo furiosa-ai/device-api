a70b18aadcbe6ca0626b3f703f372597f7cdfe9d
Yongwoo Lee <ywlee@furiosa.ai>
2023-04-06 15:10:03 +0900

    add list_devices, find_devices

--- device-api-python/src/lib.rs
+++ device-api-python/src/lib.rs
@@ -1,4 +1,5 @@
-use furiosa_device::list_devices;
+use device::DeviceFilePy;
+use furiosa_device::{find_devices, get_device, list_devices};
 use pyo3::prelude::*;
 
 mod arch;
@@ -7,15 +8,41 @@ mod device;
 mod errors;
 mod hwmon;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyfunction(name = "list_devices")]
+fn list_devices_python(py: Python) -> PyResult<&PyAny> {
+    pyo3_asyncio::tokio::future_into_py(py, async move {
+        list_devices()
+            .await
+            .map(|list| {
+                list.iter()
+                    .map(|d| device::DevicePy::new(d.clone()))
+                    .collect::<Vec<device::DevicePy>>()
+            })
+            .map_err(errors::to_py_err)
+    })
+}
+
+#[pyfunction(name = "find_devices")]
+fn find_devices_python(py: Python, config: config::DeviceConfigPy) -> PyResult<&PyAny> {
+    pyo3_asyncio::tokio::future_into_py(py, async move {
+        find_devices(&config.inner)
+            .await
+            .map(|list| {
+                list.iter()
+                    .map(|d| device::DeviceFilePy::new(d.clone()))
+                    .collect::<Vec<DeviceFilePy>>()
+            })
+            .map_err(errors::to_py_err)
+    })
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn device_api_python(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn furiosa_device_python(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<arch::ArchPy>();
+    m.add_class::<device::DeviceModePy>();
+    m.add_class::<config::DeviceConfigPy>();
+    m.add_function(wrap_pyfunction!(list_devices_python, m)?)?;
+    m.add_function(wrap_pyfunction!(find_devices_python, m)?)?;
     Ok(())
 }
