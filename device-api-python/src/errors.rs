a055b94cbdcb61348390fac79cb5bd11db3da245
Yongwoo Lee <ywlee@furiosa.ai>
2023-04-07 10:08:57 +0900

    remove unused function

--- device-api-python/src/errors.rs
+++ device-api-python/src/errors.rs
@@ -1,12 +1,5 @@
-use furiosa_device::{DeviceError, DeviceResult};
-use pyo3::{exceptions::PyRuntimeError, PyErr, PyResult};
-
-pub fn to_py_result<T>(result: DeviceResult<T>) -> PyResult<T> {
-    match result {
-        Ok(r) => Ok(r),
-        Err(e) => Err(to_py_err(e)),
-    }
-}
+use furiosa_device::{DeviceError};
+use pyo3::{exceptions::PyRuntimeError, PyErr};
 
 pub fn to_py_err(err: DeviceError) -> PyErr {
     PyRuntimeError::new_err(err.to_string())
