a3b95c06d54c5f22048b3f519304d5c4a77ae64b
Yongwoo Lee <ywlee@furiosa.ai>
2023-04-07 10:16:20 +0900

    replace tokio runtime to pyo3_asyncio

--- device-api-python/src/errors.rs
+++ device-api-python/src/errors.rs
@@ -1,4 +1,4 @@
-use furiosa_device::{DeviceError};
+use furiosa_device::DeviceError;
 use pyo3::{exceptions::PyRuntimeError, PyErr};
 
 pub fn to_py_err(err: DeviceError) -> PyErr {
--- device-api-python/src/hwmon.rs
+++ device-api-python/src/hwmon.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use furiosa_device::hwmon::{Fetcher, SensorValue};
 use pyo3::prelude::*;
 use tokio::runtime::Runtime;
@@ -6,7 +8,9 @@ use crate::errors::to_py_err;
 
 #[pyclass(name = "SensorValue")]
 pub struct SensorValuePy {
+    #[pyo3(get)]
     label: String,
+    #[pyo3(get)]
     value: i32,
 }
 
@@ -21,58 +25,76 @@ impl SensorValuePy {
 
 #[pyclass(name = "Fetcher")]
 pub struct FetcherPy {
-    inner: Fetcher,
+    inner: Arc<Fetcher>,
 }
 
 impl FetcherPy {
     pub fn new(f: &Fetcher) -> Self {
-        Self { inner: f.clone() }
+        Self {
+            inner: Arc::new(f.clone()),
+        }
     }
 }
 
 #[pymethods]
 impl FetcherPy {
-    pub fn read_currents(&self) -> PyResult<Vec<SensorValuePy>> {
-        let currents = Runtime::new()
-            .unwrap()
-            .block_on(self.inner.read_currents())
-            .map_err(to_py_err);
-        match currents {
-            Ok(c) => Ok(c.iter().map(SensorValuePy::new).collect()),
-            Err(e) => Err(e),
-        }
+    pub fn read_currents<'py, 'a>(&'a self, py: Python<'py>) -> PyResult<&'py PyAny> {
+        let fetcher = self.inner.clone();
+        pyo3_asyncio::tokio::future_into_py(py, async move {
+            fetcher
+                .read_currents()
+                .await
+                .map(|vec| {
+                    vec.iter()
+                        .map(SensorValuePy::new)
+                        .collect::<Vec<SensorValuePy>>()
+                })
+                .map_err(to_py_err)
+        })
     }
 
-    pub fn read_voltages(&self) -> PyResult<Vec<SensorValuePy>> {
-        let voltages = Runtime::new()
-            .unwrap()
-            .block_on(self.inner.read_voltages())
-            .map_err(to_py_err);
-        match voltages {
-            Ok(v) => Ok(v.iter().map(SensorValuePy::new).collect()),
-            Err(e) => Err(e),
-        }
+    pub fn read_voltages<'py, 'a>(&'a self, py: Python<'py>) -> PyResult<&'py PyAny> {
+        let fetcher = self.inner.clone();
+        pyo3_asyncio::tokio::future_into_py(py, async move {
+            fetcher
+                .read_voltages()
+                .await
+                .map(|vec| {
+                    vec.iter()
+                        .map(SensorValuePy::new)
+                        .collect::<Vec<SensorValuePy>>()
+                })
+                .map_err(to_py_err)
+        })
     }
 
-    pub fn read_powers_average(&self) -> PyResult<Vec<SensorValuePy>> {
-        let powers = Runtime::new()
-            .unwrap()
-            .block_on(self.inner.read_powers_average())
-            .map_err(to_py_err);
-        match powers {
-            Ok(p) => Ok(p.iter().map(SensorValuePy::new).collect()),
-            Err(e) => Err(e),
-        }
+    pub fn read_powers_average<'py, 'a>(&'a self, py: Python<'py>) -> PyResult<&'py PyAny> {
+        let fetcher = self.inner.clone();
+        pyo3_asyncio::tokio::future_into_py(py, async move {
+            fetcher
+                .read_powers_average()
+                .await
+                .map(|vec| {
+                    vec.iter()
+                        .map(SensorValuePy::new)
+                        .collect::<Vec<SensorValuePy>>()
+                })
+                .map_err(to_py_err)
+        })
     }
 
-    pub fn read_temperatures(&self) -> PyResult<Vec<SensorValuePy>> {
-        let temperatures = Runtime::new()
-            .unwrap()
-            .block_on(self.inner.read_temperatures())
-            .map_err(to_py_err);
-        match temperatures {
-            Ok(t) => Ok(t.iter().map(SensorValuePy::new).collect()),
-            Err(e) => Err(e),
-        }
+    pub fn read_temperatures<'py, 'a>(&'a self, py: Python<'py>) -> PyResult<&'py PyAny> {
+        let fetcher = self.inner.clone();
+        pyo3_asyncio::tokio::future_into_py(py, async move {
+            fetcher
+                .read_temperatures()
+                .await
+                .map(|vec| {
+                    vec.iter()
+                        .map(SensorValuePy::new)
+                        .collect::<Vec<SensorValuePy>>()
+                })
+                .map_err(to_py_err)
+        })
     }
 }
