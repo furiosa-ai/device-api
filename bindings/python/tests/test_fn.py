810e00091c27fb9771bd9f2a82f17ec7c3ba07ec
Yongwoo Lee <ywlee@furiosa.ai>
2023-04-26 11:00:57 +0900

    fix tests

--- bindings/python/tests/test_fn.py
+++ bindings/python/tests/test_fn.py
@@ -11,27 +11,27 @@ from furiosa_device import (
 )
 
 
-def get_first_device_name():
-    return sorted(glob.glob("/dev/npu*"))[0].split("/")[-1]
+def get_first_device_name(pattern):
+    return sorted(glob.glob(pattern))[0].split("/")[-1]
 
 
 @pytest.mark.asyncio
 async def test_list_devices():
-    dev_name = get_first_device_name()
+    dev_name = get_first_device_name("/dev/npu*")
     devices = await list_devices()
     assert devices[0].name() == dev_name
 
 
 @pytest.mark.asyncio
 async def test_find_devices():
-    dev_name = get_first_device_name()
+    dev_name = get_first_device_name("/dev/npu*pe0-1")
     config = DeviceConfig(arch=Arch.Warboy, mode=DeviceMode.Fusion, count=1)
     devices = await find_devices(config)
-    assert devices[0].filename() == f"{dev_name}pe0-1"
+    assert devices[0].filename() == dev_name
 
 
 @pytest.mark.asyncio
 async def test_get_device():
-    dev_name = get_first_device_name()
-    device = await get_device(f"{dev_name}pe1")
-    assert device.filename() == f"{dev_name}pe1"
+    dev_name = get_first_device_name("/dev/npu*pe1")
+    device = await get_device(dev_name)
+    assert device.filename() == dev_name
--- bindings/python/tests/test_fn_sync.py
+++ bindings/python/tests/test_fn_sync.py
@@ -4,24 +4,24 @@ from furiosa_device import Arch, DeviceConfig, DeviceMode
 from furiosa_device.sync import find_devices, get_device, list_devices
 
 
-def get_first_device_name():
-    return sorted(glob.glob("/dev/npu*"))[0].split("/")[-1]
+def get_first_device_name(pattern):
+    return sorted(glob.glob(pattern))[0].split("/")[-1]
 
 
 def test_list_devices():
-    dev_name = get_first_device_name()
+    dev_name = get_first_device_name("/dev/npu*")
     devices = list_devices()
     assert devices[0].name() == dev_name
 
 
 def test_find_devices():
-    dev_name = get_first_device_name()
+    dev_name = get_first_device_name("/dev/npu*pe0-1")
     config = DeviceConfig(arch=Arch.Warboy, mode=DeviceMode.Fusion, count=1)
     devices = find_devices(config)
-    assert devices[0].filename() == f"{dev_name}pe0-1"
+    assert devices[0].filename() == dev_name
 
 
 def test_get_device():
-    dev_name = get_first_device_name()
-    device = get_device(f"{dev_name}pe1")
-    assert device.filename() == f"{dev_name}pe1"
+    dev_name = get_first_device_name("/dev/npu*pe1")
+    device = get_device(dev_name)
+    assert device.filename() == dev_name
