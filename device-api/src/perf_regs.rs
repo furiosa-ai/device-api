66a80ebcb3a2c20a1bacbfeb2f8d90852932289a
ByungChan Lee <libc@furiosa.ai>
2023-06-29 20:26:10 +0900

    update performance counter APIs

--- device-api/src/perf_regs.rs
+++ device-api/src/perf_regs.rs
@@ -30,13 +30,13 @@ pub mod error {
 }
 
 #[derive(Debug, Default, Clone, Copy)]
-pub struct PerformanceValues {
+pub struct Utilization {
     npu_utilization: f64,
     computation_ratio: f64,
     io_ratio: f64,
 }
 
-impl PerformanceValues {
+impl Utilization {
     fn new(npu_utilization: f64, computation_ratio: f64, io_ratio: f64) -> Self {
         Self {
             npu_utilization,
@@ -136,45 +136,116 @@ impl PerformanceCounter {
         Ok(counter)
     }
 
-    /// Calculate utilization using performance counters at two points in time
-    pub fn calculate_performance_values(&self, other: &Self) -> Option<PerformanceValues> {
+    pub fn cycle_count(&self) -> usize {
+        self.cycle_count
+    }
+
+    pub fn task_execution_cycle(&self) -> u32 {
+        self.task_execution_cycle
+    }
+
+    pub fn tensor_execution_cycle(&self) -> u32 {
+        self.tensor_execution_cycle
+    }
+
+    pub fn calculate_increased(&self, other: &Self) -> Self {
         let (prev, next) = if self.now < other.now {
             (self, other)
         } else {
             (other, self)
         };
 
-        // when the cycle count is reversed, it is considered invalid.
+        // when the cycle count is reversed, NPU was restarted.
         if next.cycle_count < prev.cycle_count {
-            return None;
+            return next.clone();
         }
 
+        let elapsed_time = next.now.duration_since(prev.now);
+        if elapsed_time.is_err() {
+            return next.clone();
+        }
+
+        let elapsed_time = elapsed_time.unwrap().as_nanos() as usize;
         let cycle_gap = next.cycle_count - prev.cycle_count;
+
+        // Unfortunately, it's a naive implementation for now.
+        if (cycle_gap as f64 / elapsed_time as f64) < 0.45 {
+            return next.clone();
+        }
+
         let task_cycle_gap =
             Self::safe_u32_subtract(next.task_execution_cycle, prev.task_execution_cycle);
         let tensor_cycle_gap =
             Self::safe_u32_subtract(next.tensor_execution_cycle, prev.tensor_execution_cycle);
 
-        // If the task cycle is bigger than the total cycle, it is considered invalid.
-        if cycle_gap < task_cycle_gap {
-            return None;
+        let task_cycle_gap = if cycle_gap < task_cycle_gap {
+            cycle_gap
+        } else {
+            task_cycle_gap
+        };
+
+        let tensor_cycle_gap = if task_cycle_gap < tensor_cycle_gap {
+            task_cycle_gap
+        } else {
+            tensor_cycle_gap
+        };
+
+        Self {
+            now: next.now,
+            cycle_count: cycle_gap,
+            task_execution_cycle: if task_cycle_gap > u32::MAX as usize {
+                u32::MAX
+            } else {
+                task_cycle_gap as u32
+            },
+            tensor_execution_cycle: if tensor_cycle_gap > u32::MAX as usize {
+                u32::MAX
+            } else {
+                tensor_cycle_gap as u32
+            },
+        }
+    }
+
+    /// Calculate utilization using performance counters at two points in time
+    pub fn calculate_utilization(&self, other: &Self) -> Option<Utilization> {
+        // let (prev, next) = if self.now < other.now {
+        //     (self, other)
+        // } else {
+        //     (other, self)
+        // };
+
+        // // when the cycle count is reversed, it is considered invalid.
+        // if next.cycle_count < prev.cycle_count {
+        //     return None;
+        // }
+
+        // let cycle_gap = next.cycle_count - prev.cycle_count;
+        // let task_cycle_gap =
+        //     Self::safe_u32_subtract(next.task_execution_cycle, prev.task_execution_cycle);
+        // let tensor_cycle_gap =
+        //     Self::safe_u32_subtract(next.tensor_execution_cycle, prev.tensor_execution_cycle);
+
+        // // If the task cycle is bigger than the total cycle, it is considered invalid.
+        // if cycle_gap < task_cycle_gap {
+        //     return None;
+        // }
+        let diff = self.calculate_increased(other);
+
+        if diff.task_execution_cycle == 0 {
+            return Some(Utilization::default());
         }
 
-        if task_cycle_gap == 0 {
-            return Some(PerformanceValues::default());
-        }
-
-        let npu_utilization = Self::safe_usize_divide(task_cycle_gap, cycle_gap);
+        let npu_utilization =
+            Self::safe_usize_divide(diff.task_execution_cycle as usize, diff.cycle_count);
 
         // Sometimes the tensor cycle is larger than the task cycle due to observation timing issues. In this case, an upper bound is applied.
-        let computation_ratio = if task_cycle_gap < tensor_cycle_gap {
-            1.0
-        } else {
-            Self::safe_usize_divide(tensor_cycle_gap, task_cycle_gap)
-        };
+        let computation_ratio = Self::safe_usize_divide(
+            diff.tensor_execution_cycle as usize,
+            diff.task_execution_cycle as usize,
+        );
         let io_ratio = 1.0 - computation_ratio;
 
-        Some(PerformanceValues::new(
+        Some(Utilization::new(
             npu_utilization,
             computation_ratio,
             io_ratio,
@@ -336,7 +407,7 @@ CycleCountHigh: 0x0
         assert!(res.is_ok());
         let next = res.unwrap();
 
-        let res = next.calculate_performance_values(&prev);
+        let res = next.calculate_utilization(&prev);
         assert!(res.is_some());
 
         let values = res.unwrap();
