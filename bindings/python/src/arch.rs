c72d72623120e07dcd5e9bc903f6f1b65e935060
Sungyoon Jeong <sungyoon.jeong@furiosa.ai>
2024-04-15 13:53:26 +0900

    Remove unnecessary comment in bindings/python/src/arch.rs

--- bindings/python/src/arch.rs
+++ bindings/python/src/arch.rs
@@ -4,7 +4,6 @@ use pyo3::prelude::*;
 #[pyclass(name = "Arch")]
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum ArchPy {
-    // It indicates WarboyB0 since WarboyB0 is the default
     Warboy,
     Renegade,
 }
