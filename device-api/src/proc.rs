0c8ab50611d42b06112ef4096258aa9da7eb1b31
bg-furiosa <bg.chun@furiosa.ai>
2024-04-23 18:35:05 +0900

    disable import for non linux test
    Signed-off-by: bg-furiosa <bg.chun@furiosa.ai>

--- device-api/src/proc.rs
+++ device-api/src/proc.rs
@@ -99,6 +99,9 @@ fn read_cmdline(pid: u32) -> Option<String> {
 
 #[cfg(test)]
 mod tests {
+    #[cfg(target_os = "linux")]
+    use super::*;
+
     #[test]
     #[cfg(target_os = "linux")]
     fn read_cmdline_test() {
